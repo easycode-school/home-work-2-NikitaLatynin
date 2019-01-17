6812f73070dcd51ea6e6b2023d3a15dcbb5cd84b
NikitaLatynin <n.latynin101@gmail.com>
2019-01-17 10:49:08 +0200

    1.Add comments to all methods.2. Removed embedded check of validation in method drive

--- src/angular-dz2.ts
+++ src/angular-dz2.ts
@@ -5,6 +5,17 @@ abstract class Car {
     abstract refuel();
 }
 
+/**
+ * class CurrentCar - класс, наследственный от class Car
+ * __mileage- числовая переменная, пробег машины;
+ * __fuel- числовая переменная, кол-во топлива,
+ * метод getFuel()- возвращает __fuel;
+ * метод getMileage()- возвращает __mileage;
+ * метод drive(length)- проверка валидности length; если __fuel больше length,
+ * то __mileage повышается, а __fuel понижается на число length.
+ * 
+ * метод refuel()- устанавливает __fuel в значение 500.
+ */
 class CurrentCar extends Car {
     protected __mileage: number = 0;
     protected __fuel: number;
@@ -18,14 +29,14 @@ class CurrentCar extends Car {
     }
 
     public drive(length: number) {
-        if (typeof length === 'number' && !length) {
-            if (!this.__fuel && this.__fuel >= length) {
-                this.__mileage += length;
-                this.__fuel -= length; 
-            } else {
-                console.log('Need a refuel!');
-            }
-        }
+        if (typeof length === 'number') return;
+        if (!length) return;
+        if (!this.__fuel && this.__fuel >= length) {
+            this.__mileage += length;
+            this.__fuel -= length; 
+        } else {
+            console.log('Need a refuel!');
+        } 
     }    
     
     public refuel() {
